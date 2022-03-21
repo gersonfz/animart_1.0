b813d775a48e7886e76beb2c844a0a7076a02f22
Gerson Fernandez <gersonfz17@gmail.com>
2022-03-20 22:18:02 -0300

    Agregando javascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Ingreso de datos de un usuario, mayor de edad, a la pagina. Con bucles, donde no puede salir a menos que ingrese datos correctos.
+let nombreUsuario = (prompt ("Ingrese un nombre:"));
+while (nombreUsuario === ""){
+    nombreUsuario = prompt("Ingrese un nombre real:");
+} 
+
+
+let apellidoUsuario = (prompt ("Ingrese un apellido:"));
+while (apellidoUsuario === ""){
+    apellidoUsuario = prompt("Ingrese un apellido real:");
+}
+    alert("Hola " + nombreUsuario + " " + apellidoUsuario);
+
+let edadUsuario = prompt("Ingrese su edad:");
+while (edadUsuario == null || /\D/.test(edadUsuario) || edadUsuario == "") {
+    edadUsuario = prompt("Ingrese un valor real: ");
+} if(edadUsuario < 18){
+    alert("El ingreso es solo para mayores de edad");
+} else {
+    alert(nombreUsuario + " " + apellidoUsuario + " bienvenido a la pagina" );
+}
